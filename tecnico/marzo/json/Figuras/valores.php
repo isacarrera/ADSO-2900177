898c68f68c53d2e6f79d33d74ea04595d9eb6b23
isabella carrera <142624272+isacarrera@users.noreply.github.com>
2024-10-04 00:17:00 -0500

    Add files via upload

---
+++ tecnico/marzo/json/Figuras/valores.php
@@ -0,0 +1,13 @@
+<?php
+class Valores {
+    public $lado;
+    public $base;
+    public $altura;
+
+    public function __construct($lado, $base, $altura) {
+        $this->lado = $lado;
+        $this->base = $base;
+        $this->altura = $altura;
+    }
+}
+?>
---
+++ tecnico/marzo/json/opera/numero.php
@@ -0,0 +1,16 @@
+<?php
+class Numero{
+    private $valor;
+    
+    public function __construct($valor){
+        $this->valor = $valor;
+    }
+    public function setValor($valor) { 
+        $this->valor = $valor;
+    } 
+    public function getValor(){
+        return $this->valor;
+       }
+}
+?>
+
